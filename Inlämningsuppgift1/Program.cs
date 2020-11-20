1d91c865fb59cf4f297152d98252b2fcba5046ef
David Frödin <dawid.frodin@gmail.com>
2020-11-20 10:43:21 +0100

    V.1

--- Inlämningsuppgift1/Program.cs
+++ Inlämningsuppgift1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Inlämningsuppgift1
 {
@@ -6,7 +7,88 @@ namespace Inlämningsuppgift1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            String end = "yes";
+
+            while (end != "no")
+            {
+                Console.Write("> Enter the first operator: ");
+                String o1 = Console.ReadLine();
+
+                Console.Write("> Enter the second operator: ");
+                String o2 = Console.ReadLine();
+
+                Console.Write("> Enter the first term: ");
+                String t1 = Console.ReadLine();
+
+                Console.Write("> Enter the second term: ");
+                String t2 = Console.ReadLine();
+
+                Console.Write("> Enter the third term: ");
+                String t3 = Console.ReadLine();
+
+                int t1Int = Convert.ToInt32(t1);
+                int t2Int = Convert.ToInt32(t2);
+                int t3Int = Convert.ToInt32(t3);
+                int sum;
+
+                List<int> sumList = new List<int>();
+
+                if (o1 == "+" && o2 == "+")
+                {
+                    sum = (t1Int + t1Int) + t3Int;
+                    MagicTextEquation(o1, o2, t1, t2, t3, sum);
+                    MagicTextSum(sum);
+                    sumList.Add(sum);
+                }
+
+                else if (o1 == "-" && o2 == "+")
+                {
+                    sum = (t1Int - t1Int) + t3Int;
+                    MagicTextEquation(o1, o2, t1, t2, t3, sum);
+                    MagicTextSum(sum);
+                    sumList.Add(sum);
+                }
+
+                Console.Write("> Do you want to continue? 1. yes 2. no: ");
+                end = Console.ReadLine();
+
+            }
+
+
+
+
+
+
+
+        }
+
+        static void MagicTextSum (int s)
+        {
+            if (s < 100)
+            {
+                Console.WriteLine("> The sum is less then a hundred");
+            }
+
+            else if (s == 100)
+            {
+                Console.WriteLine("> Cool, know you have a hundred, clap clap");
+
+            }
+
+            else if (s > 100)
+            {
+                Console.WriteLine("> The sum is more than a hundred");
+            }
+
+
+        }
+
+        static void MagicTextEquation (string o1, string o2, string t1, string t2, string t3, int sum)
+        {
+            Console.WriteLine("> " + t1 + " " + o1 + " " + t2 + " " + o2 + " " + t3 + " " + "= " + sum);
+
         }
     }
+
+   
 }
